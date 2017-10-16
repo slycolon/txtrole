50e21de6e23f93d6cca1bb382850fa98406e5262
slycolon <jasonphillips130@gmail.com>
2017-10-16 12:31:11 -0500

    added cmds() functions

--- tables.cpp
+++ tables.cpp
@@ -5,3 +5,19 @@
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
+
+void cmds()         		    // used as a list of commands
+{
+	cout<< "command                                       what it does" << endl
+		<< "--------------------------------------------------------------" << endl
+		<< "n                                                 go north" << endl
+		<< "s                                                 go south" << endl
+		<< "e                                                 go east" << endl
+		<< "w                                                 go west" << endl
+		<< "i                                         check inventory" << endl
+		<< "attack 'x'                                attack an enemy" << endl
+		<< "heal                          gives you 5 hp, uses 5 mana" << endl
+		<< "                             mana can only be restored at" << endl
+		<< "                                      fountains of youth)" << endl
+		<< "--------------------------------------------------------------" << endl;
+}
