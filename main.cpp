07a3190cd2b881a0c8b2f273d31456c37fd073ef
slycolon <jasonphillips130@gmail.com>
2017-11-19 16:19:38 -0600

    added findName() & global stats
    findName() asks the player for their name and confirms that the name they gave is correct
    
    also added global statistics for the players, such as strength and speed, to calculate combat mechanics later on.

--- main.cpp
+++ main.cpp
@@ -6,12 +6,18 @@
 using namespace std;
 
 //begin global variables
-
+string name;
+int health = 50;
+int strength = 15;
+int defense = 15;
+int speed = 15;
+int level = 1;
+int experience = 0;
 //end global variables
 
 //begin function prototyping
-
-
+void begin();
+void findName();
 //end function prototyping
 
 int main()
@@ -19,6 +25,7 @@ int main()
 	char choice;
 	
 	begin();
+	findName();
 }
 
 
@@ -36,4 +43,40 @@ void begin();
 		<<"  |__|/_/\_\|__| |_|   |_____|__|_____ " << endl << endl
 		<<"welcome to txtrole, a text-based choose your own adventure style rpg," << endl
 		<<"created by slycolon. press enter to begin. ";
+	cin.ignore();
+	cout<<endl;
 }
+
+/****
+function name: void findName()
+purpose: asks the player for their name
+****/
+
+void findName()
+{
+	bool askName = true;
+	char isName;
+	
+	cout<<"now, to start out, what is your name, traveller?" << endl << endl
+		<<">";
+	cin >> name;
+	
+	do
+	{
+		cout<<"so, your name is " << name << " (y or n)?" << endl << endl << ">";
+		
+		cin >> isName;
+		
+		if (isName == 'y' || isName == 'Y')
+		{
+			askName = true;						//if they say yes, the exit condition is fulfilled.
+		}
+		else
+		{
+			askName = false;
+			cout<<endl <<"okay, so if your name isn't " << name << ", then what might it be?" << endl << endl ">";
+			
+			cin >> name;
+		}
+		
+	} while (askName == false);
