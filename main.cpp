f58fce5ac6ecff9400990a1a4ce1cd3f38bec9fb
slycolon <jasonphillips130@gmail.com>
2017-11-19 15:29:23 -0600

    restarted program, wrote main() and begin()

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+//begin global variables
+
+//end global variables
+
+//begin function prototyping
+
+
+//end function prototyping
+
+int main()
+{
+	char choice;
+	
+	begin();
+}
+
+
+/*****************
+function name: void begin()
+purpose: begins the game
+*****************/
+void begin();
+{
+	cout<<"© slycolon 2017. all rights reserved." << endl << endl
+		<<" ______                       " << endl
+		<<"|_    _|     __               __" << endl
+		<<"  |  |__  __|  |_ ____  _____|  |_____ " << endl
+		<<"  |  |\ \/ /|   _|  __||  .  |  |  -__|" << endl
+		<<"  |__|/_/\_\|__| |_|   |_____|__|_____ " << endl << endl
+		<<"welcome to txtrole, a text-based choose your own adventure style rpg," << endl
+		<<"created by slycolon. press enter to begin. ";
+}
