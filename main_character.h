c6a7081a750e315c9e0413cefdb1c20a285bc6a5
slycolon <jasonphillips130@gmail.com>
2018-02-26 09:28:01 -0600

    Update main_character.h

--- main_character.h
+++ main_character.h
@@ -3,7 +3,41 @@
 
 class player
 {
-
+	private:
+		int health;
+		int attack;
+		int defense;
+		
+	public:
+		int get_health()
+		{
+			return health;
+		}
+		
+		int get_attack()
+		{
+			return attack;
+		}
+	
+		int get_defense()
+		{
+			return defense;
+		}
+	
+		void set_health(int sethealth)
+		{
+			health = sethealth;
+		}
+	
+		void set_attack(int setattack)
+		{
+			attack = setattack;
+		}
+	
+		void set_defense(int setdefense)
+		{
+			defense = setdefense;
+		}
 }
 
 
