d1b329ce8cc4c14657bee844179904b02b2778f7
slycolon <jasonphillips130@gmail.com>
2018-02-27 09:30:03 -0600

    added get&set methods

--- minion.h
+++ minion.h
@@ -3,7 +3,73 @@
 
 class minion
 {
-
+	private:
+		int type;
+		int health;
+		int attack;
+		int atkModifier;
+		int defense;
+		int speed;
+	public:
+		int get_type()
+		{
+			return type;
+		}
+	
+		void set_type(int settype)
+		{
+			type = settype;
+		}
+	
+		int get_health()
+		{
+			return health;
+		}
+		
+		void set_health(int sethealth)
+		{
+			health = sethealth;
+		}
+		
+		int get_attack()
+		{
+			return attack;
+		}
+		
+		void set_attack(int setattack)
+		{
+			attack = setattack;
+		}
+	
+		int get_atkModifier()
+		{
+			return atkModifier;
+		}
+	
+		void set_atkModifier(int setatkmodifier)
+		{
+			atkModifier = setatkmodifier;
+		}
+		
+		int get_defense()
+		{
+			return defense;
+		}
+		
+		void set_defense(int setdefense)
+		{
+			defense = setdefense;
+		}
+		
+		int get_speed()
+		{
+			return speed;
+		}
+		
+		void set_speed(int setspeed)
+		{
+			speed = setspeed;
+		}
 }
 
 #endif
